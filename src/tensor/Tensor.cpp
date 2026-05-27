b2ad8f56c263adb581a5ea49f815e4e1a4329cdd
Sohum Guha <sohumguha@gmail.com>
2026-05-27 10:55:37 -0400

    implemented compute_strides

--- src/tensor/Tensor.cpp
+++ src/tensor/Tensor.cpp
@@ -2,6 +2,29 @@
 
 namespace tc {
 
+    void Tensor::compute_strides(){
+        // check shape is non empty
+        if (shape_.empty()) return;
+
+        size_ = 1; // make sure size starts at 1 and not 0
+        strides_.resize(shape_.size()); // initialize strides_ vector to have same elements as shape
+        // last element of stride is always 1
+        strides_[shape_.size() - 1] = 1;
+
+        // compute total num of elements
+        for(size_t i = 0; i < shape_.size(); i++) {
+            size_ *= shape_[i];
+        }
+
+        // compute strides
+        for(size_t i = shape_.size() - 1; i > 0; i --){
+            strides_[i-1] = strides_[i] * shape_[i];
+        }
+    }
+
+    // constructor
+    
+
 
 
 } 
