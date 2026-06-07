ae2742871fea8b9adb1e6e70f2d145e2fe90dd19
Sohum Guha <sohumguha@gmail.com>
2026-06-07 04:20:19 -0400

    added header file for lexer

---
+++ include/tc/parser/Lexer.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <string>
+#include <vector>
+
+namespace tc {
+
+    enum TokenKind{
+        LPAREN, RPAREN, COMMA, IDENT, TOK_EOF
+    };
+
+    struct Token{
+        TokenKind kind;
+        std::string lexeme;
+        int line;
+    };
+
+    class Lexer {
+        public:
+        Lexer(const std::string& source);
+        std::vector<Token> tokenize() ;
+
+        private: 
+        std::string source_;
+        int pos_;
+        int line_;
+    };
+
+}
