7d7a7a1a55c3c3a7f4affdd4d9479f552a0c662a
Sohum Guha <sohumguha@gmail.com>
2026-08-10 16:36:46 -0400

    Add hand-written dialect registration (TensorDialect.h/.cpp); tc_dialect builds

---
+++ include/tc/dialect/TensorDialect.h
@@ -0,0 +1,8 @@
+#ifndef TC_DIALECT_TENSORDIALECT_H
+#define TC_DIALECT_TENSORDIALECT_H
+
+#include "mlir/IR/Dialect.h"
+
+#include "TensorDialect.h.inc"
+
+#endif // TC_DIALECT_TENSORDIALECT_H
---
+++ src/dialect/TensorDialect.cpp
@@ -0,0 +1,7 @@
+#include "tc/dialect/TensorDialect.h"
+
+#include "TensorDialect.cpp.inc"
+
+void mlir::tc::TensorDialect::initialize() {
+  // Op registration will go here once ops exist.
+}
