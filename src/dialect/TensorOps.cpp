eeefdf8af82673abe022c096cbfad4b0ad49e63b
Sohum Guha <sohumguha@gmail.com>
2026-08-11 18:41:28 -0400

    Add MatMulOp verifier: check inner dimension compatibility

--- src/dialect/TensorOps.cpp
+++ src/dialect/TensorOps.cpp
@@ -2,3 +2,19 @@
 
 #define GET_OP_CLASSES
 #include "TensorOps.cpp.inc"
+
+::llvm::LogicalResult mlir::tc::MatMulOp::verify() {
+
+    auto lhsType = ::llvm::cast<mlir::RankedTensorType>(getLhs().getType());
+    auto rhsType = ::llvm::cast<mlir::RankedTensorType>(getRhs().getType());
+
+    auto lhsShape = lhsType.getShape();
+    auto rhsShape = rhsType.getShape();
+
+    // check if its (M,N) x (N,M) for valid multiplication
+    if (lhsShape[1] != rhsShape[0]) {
+        return emitOpError("inner dimension must match: lhs has ") << lhsShape[1] << ", rhs has " << rhsShape[0];
+    }
+
+  return ::llvm::success();
+}
